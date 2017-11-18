f1fef54ae54494add13a242e703921ec99d4f247
Sai Teja Suggula <suggulasaiteja@gmail.com>
2017-11-18 10:00:32 +0530

    Create MainActivity.java

---
+++ ContentProvider/MainActivity.java
@@ -0,0 +1,91 @@
+package example.sai.readcontacts;
+
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private ProgressDialog pDialog;
+    private Handler updateBarHandler;
+    ArrayList<String> contactList;
+    Cursor cursor;
+    int counter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.list);
+
+        getContacts();
+        ArrayAdapter ar=new ArrayAdapter(this,android.R.layout.simple_list_item_1,contactList);
+        mListView.setAdapter(ar);
+        pDialog.cancel();
+        // Set onclicklistener to the list item.
+        mListView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                //TODO Do whatever you want with the list data
+                Toast.makeText(getApplicationContext(), "item clicked : \n"+contactList.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void getContacts()
+    {
+        contactList = new ArrayList<String>();
+        String phoneNumber = null;
+        Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+        String _ID = ContactsContract.Contacts._ID;
+        String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+        String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+        Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+        String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+
+        StringBuffer output;
+        ContentResolver contentResolver = getContentResolver();
+        cursor = contentResolver.query(CONTENT_URI, null,null, null, null);
+        if (cursor.getCount() > 0) {
+            counter = 0;
+            while (cursor.moveToNext()) {
+                output = new StringBuffer();
+
+                String contact_id = cursor.getString(cursor.getColumnIndex( _ID ));
+                String name = cursor.getString(cursor.getColumnIndex( DISPLAY_NAME ));
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex( HAS_PHONE_NUMBER )));
+                if (hasPhoneNumber > 0) {
+                    output.append(" Name:"+ name);
+                    Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[] { contact_id }, null);
+                }
+
+                Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[] { contact_id }, null);
+                while (phoneCursor.moveToNext()) {
+                    phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
+                    output.append("\n Phone number:" + phoneNumber);
+                }
+                phoneCursor.close();
+                contactList.add(output.toString());
+            }
+
+
+        }
+    }
+
+}
